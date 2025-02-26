685b8c5413d99a3159f6ab881883eebfd889eff3
adriansyah3306 <149454504+adriansyah3306@users.noreply.github.com>
2025-02-26 08:59:38 +0700

    Add files via upload

---
+++ db_adi.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2025 at 02:56 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_adi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `name`, `password`, `created_at`) VALUES
+(6, 'muhammadadriansyah930@gmail.com', 'adi', '$2y$10$AJhYgCjzLif87tmIjLaBg.CqBMDRR7oV.AWPrK77H1p7SgWaIGxnS', '2025-02-24 02:59:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun_mall`
+--
+
+CREATE TABLE `akun_mall` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(231) NOT NULL,
+  `nama_mall` varchar(231) NOT NULL,
+  `nik` varchar(231) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `akun_mall`
+--
+
+INSERT INTO `akun_mall` (`id`, `email`, `password`, `nama_mall`, `nik`) VALUES
+(5, 'muhammadadriansyah930@gmail.com', '12345', 'Transpark Bandung', '12345'),
+(6, '', '', 'Ciputra', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id` int(11) NOT NULL,
+  `poster` varchar(255) NOT NULL,
+  `banner` varchar(231) NOT NULL,
+  `trailer` varchar(231) NOT NULL,
+  `nama_film` varchar(231) NOT NULL,
+  `judul` longtext NOT NULL,
+  `total_menit` varchar(231) NOT NULL,
+  `usia` varchar(231) NOT NULL,
+  `genre` varchar(231) NOT NULL,
+  `dimensi` varchar(231) NOT NULL,
+  `Producer` varchar(231) NOT NULL,
+  `Director` varchar(231) NOT NULL,
+  `Writer` varchar(231) NOT NULL,
+  `Cast` varchar(231) NOT NULL,
+  `Distributor` varchar(231) NOT NULL,
+  `harga` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id`, `poster`, `banner`, `trailer`, `nama_film`, `judul`, `total_menit`, `usia`, `genre`, `dimensi`, `Producer`, `Director`, `Writer`, `Cast`, `Distributor`, `harga`) VALUES
+(13, 'uploads/poster/WhatsApp Image 2025-02-10 at 12.31.52.jpeg', 'uploads/banner/WhatsApp Image 2025-02-10 at 12.31.52.jpeg', 'uploads/trailer/SPIDER-MAN_ NO WAY HOME - Official Trailer (HD).mp4', 'SPIDERMAN ', 'Pertama kalinya dalam sejarah layar lebar Spider-Man, identitas asli dari pahlawan nan ramah ini terbongkar, sehingga membuat tanggung jawabnya sebagai seorang berkekuatan super berbenturan dengan kehidupan normalnya, dan menempatkan semua orang terdekatnya dalam posisi paling terancam.', '120', 'SU', 'Fantasy, ', '3D', 'Kevin Feige, Amy Pascal', 'Jon Watts', 'Chris McKenna, Erik Sommers', 'Tom Holland, Zendaya, Benedict Cumberbatch, Jacob Batalon, Jon Favreau, Jamie Foxx, Willem Dafoe, Alfred Molina, Benedict Wong, Tony Revolori, Marisa Tomei, Andrew Garfield, Tobey Maguire', 'Sony Pictures Releasing', '50000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_film`
+--
+
+CREATE TABLE `jadwal_film` (
+  `id` int(11) NOT NULL,
+  `mall_id` int(11) NOT NULL,
+  `film_id` int(11) NOT NULL,
+  `studio` varchar(231) NOT NULL,
+  `jam_tayang_1` time NOT NULL,
+  `jam_tayang_2` time NOT NULL,
+  `jam_tayang_3` time NOT NULL,
+  `tanggal_tayang` date NOT NULL,
+  `tanggal_akhir_tayang` date NOT NULL,
+  `total_menit` varchar(231) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jadwal_film`
+--
+
+INSERT INTO `jadwal_film` (`id`, `mall_id`, `film_id`, `studio`, `jam_tayang_1`, `jam_tayang_2`, `jam_tayang_3`, `tanggal_tayang`, `tanggal_akhir_tayang`, `total_menit`) VALUES
+(5, 5, 13, 'Studio 1', '10:20:00', '13:04:00', '17:50:00', '2025-02-26', '2025-03-26', '120');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seats`
+--
+
+CREATE TABLE `seats` (
+  `id` int(11) NOT NULL,
+  `mall_name` varchar(255) NOT NULL,
+  `seat_number` varchar(10) NOT NULL,
+  `status` enum('available','occupied') NOT NULL,
+  `film_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `order_id` varchar(50) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `payment_type` varchar(20) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `transaction_time` datetime NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `seat_number` varchar(250) NOT NULL,
+  `nama_film` varchar(231) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `created_at`) VALUES
+(2, 'muhammadadriansyah930@gmail.com', 'adi', '$2y$10$G0/r9HZBFVjMuSi.NPN0Bul32sP6Rsqoo3M1KiDujIhC6hp6DTEmu', '2025-02-24 02:55:34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`,`email`);
+
+--
+-- Indexes for table `akun_mall`
+--
+ALTER TABLE `akun_mall`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jadwal_film`
+--
+ALTER TABLE `jadwal_film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seats`
+--
+ALTER TABLE `seats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`,`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `akun_mall`
+--
+ALTER TABLE `akun_mall`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `jadwal_film`
+--
+ALTER TABLE `jadwal_film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `seats`
+--
+ALTER TABLE `seats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
